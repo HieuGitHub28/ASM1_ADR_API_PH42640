3a72103fc6e94ea9424f464e44c7415d1480add0
HieuGitHub28 <phamhieu28042004@gmail.com>
2024-03-20 11:23:39 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ASM1_ADR_API_PH42640"
+include(":app")
+ 
